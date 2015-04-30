d179ec714df27a065cb926e617d56200831bb6f9
Jure Varlec <jure@varlec.si>
2015-04-30 15:45:14 +0200

    Note the fuse settings in the openprogrammer comment.

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@
 programmer=/dev/usb/hiddev1
 dir="$(dirname $0)"
 exec "$dir"/arduwrap.sh -v --upload "$dir"/sketch/sketch.ino
-#exec op -d ATtiny84 -p "$programmer" -w "$dir"/build/sketch.cpp.hex 
+#exec op -d ATtiny84 -p "$programmer" -w "$dir"/build/sketch.cpp.hex  -fuse 0xe2 -fuseh 0xdf -fusex 0xff
