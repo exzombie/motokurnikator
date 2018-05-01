77f40cec17b0dc88dcb139f71943a18396ae3bd8
Jure Varlec <jure@varlec.si>
2018-05-01 15:38:01 +0200

    Automatic measurement of door travel time.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -17,8 +17,10 @@
  */
 
 #include <FastPin.h>
+#include <EEPROM.h>
 
-static const unsigned long maxSpinTimeMs = 5000;
+static const unsigned long maxSpinTimeMeasurementDelayMs = 10000;
+static const unsigned int blinkMs = 100;
 
 static const FastPin<3> endSwOpen;
 static const FastPin<4> endSwClosed;
@@ -34,8 +36,8 @@ static const byte dayLevelPin = A0;
 static const byte nightLevelPin = A1;
 
 static const byte standbyDelayMs = 100;
-static const byte closeDirection = -1;
-static const byte openDirection = 1;
+static const byte closeDirection = 0x00;
+static const byte openDirection = 0xff;
 
 enum class Mode: byte {
     open, closed, opening, closing
@@ -46,8 +48,12 @@ static Mode lastMode = Mode::closing;
 
 static unsigned long lastMillisBlink = 0;
 static unsigned long lastMillisMotor = 0;
+static unsigned long lastMillisManual = 0;
+static unsigned long maxSpinTimeMs = 1000;
 
-static void moveMotor(int8_t direction, bool noTimeout = false);
+static const int maxSpinTimeMsEEOffset = 1;
+
+static void moveMotor(byte direction, bool noTimeout = false);
 
 void setup()
 {
@@ -59,6 +65,14 @@ void setup()
     indicator.output();
     for_pin([](const FastAnyPin p){ p.output(); p.low(); },
         motor1, motor2);
+
+    bool isFirstRun = EEPROM[0] == 0xff;
+    if (isFirstRun) {
+      EEPROM[0] = 0;
+      EEPROM.put(maxSpinTimeMsEEOffset, maxSpinTimeMs);
+    } else {
+      EEPROM.get(maxSpinTimeMsEEOffset, maxSpinTimeMs);
+    }
 }
 
 void loop()
@@ -66,15 +80,32 @@ void loop()
     bool daytime = false, nighttime = false;
     bool override = !manualOpen.get() || !manualClose.get();
 
+    // Check whether we are at one of the end positions.
     if (!endSwOpen.get()) {
         changeMode(Mode::open);
     } else if (!endSwClosed.get()) {
         changeMode(Mode::closed);
     }
 
-    if (!manualOpen.get() && endSwClosed.get()) {
+    // Check whether the manual override has been pushed for long enough
+    // to start travel time measurement.
+    if ((!manualOpen.get() && !endSwOpen.get()) ||
+        (!manualClose.get() && !endSwClosed.get())) {
+        if (!lastMillisManual) {
+            lastMillisManual = millis();
+        } else if (millis() - lastMillisManual > maxSpinTimeMeasurementDelayMs) {
+            maxSpinTimeMeasurement();
+            lastMillisManual = 0;
+            return;
+        }
+    } else {
+        lastMillisManual = 0;
+    }
+
+    // Decide whether to open or close the door.
+    if (!manualOpen.get() && endSwOpen.get()) {
         changeMode(Mode::opening);
-    } else if (!manualClose.get() && endSwOpen.get()) {
+    } else if (!manualClose.get() && endSwClosed.get()) {
         changeMode(Mode::closing);
     } else {
         int rawLightLevel = analogRead(lightLevelPin);
@@ -100,13 +131,14 @@ void loop()
             indicator.high();
         } else {
             unsigned long ms = millis();
-            if (ms - lastMillisBlink > 100) {
+            if (ms - lastMillisBlink > blinkMs) {
                 indicator.toggle();
                 lastMillisBlink = ms;
             }
         }
     }
 
+    // Do something about the decision taken above.
     switch (mode) {
     case Mode::open:
         if (nighttime && !override) {
@@ -160,7 +192,7 @@ inline static void stopMotor()
     for_pin(lowPin, motor1, motor2);
 }
 
-static void moveMotor(int8_t direction, bool noTimeout)
+static void moveMotor(byte direction, bool noTimeout)
 {
     if (!noTimeout && millis() - lastMillisMotor > maxSpinTimeMs) {
         stopMotor();
@@ -172,11 +204,46 @@ static void moveMotor(int8_t direction, bool noTimeout)
         return;
     }
 
-    if (direction > 0) {
-      motor2.low();
-      motor1.high();
+    if (direction == openDirection) {
+        motor2.low();
+        motor1.high();
     } else {
-      motor1.low();
-      motor2.high();
+        motor1.low();
+        motor2.high();
     }
 }
+
+static void maxSpinTimeMeasurement() {
+    byte direction = mode == Mode::open ? closeDirection : openDirection;
+    unsigned long spinTime1 = millis();
+    moveMotor(direction, true);
+    if (direction == closeDirection) {
+        while (endSwClosed.get()) {}
+    } else {
+        while (endSwOpen.get()) {}
+    }
+    stopMotor();
+    spinTime1 = millis() - spinTime1;
+
+    direction = direction == closeDirection ? openDirection : closeDirection;
+    unsigned long spinTime2 = millis();
+    moveMotor(direction, true);
+    if (direction == closeDirection) {
+        while (endSwClosed.get()) {}
+    } else {
+        while (endSwOpen.get()) {}
+    }
+    stopMotor();
+    spinTime2 = millis() - spinTime2;
+
+    // Take the longer time and extend it by 5%.
+    maxSpinTimeMs = max(spinTime1, spinTime2);
+    maxSpinTimeMs *= 105;
+    maxSpinTimeMs /= 100;
+
+    // Do not accept less than 1 second.
+    maxSpinTimeMs = max(maxSpinTimeMs, 1000);
+
+    EEPROM.put(maxSpinTimeMsEEOffset, maxSpinTimeMs);
+}
+
